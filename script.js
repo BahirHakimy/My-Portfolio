15a4eaf0ab6e522a7316519f0db3ae903f1e0cdb
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-01-31 02:56:43 +0430

    Update index.html style.css Add script.js, Markup for mobile menu

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add the mobile-header class to header
+// Add the mobile-button class to hamburger nav
+// Change the class of icon tag to fa-solid fa-xmark
+// Add the mobile-nav to nav
+// Add the mobile-nav-item class to nav items

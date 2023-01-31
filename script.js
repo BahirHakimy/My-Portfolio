909157aa76926516ea5a5a27742f2edd507b6317
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-01-31 19:39:25 +0430

    Update script.js finished mobile menu

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-// Add the mobile-header class to header
 const header = document.getElementById('header');
-// Add the mobile-button class to hamburger nav
 const hamburger = document.getElementById('hamburger');
-// Change the class of icon tag to fa-solid fa-xmark
-const icon = document.querySelector('.fa-solid');
-// Add the mobile-nav to nav
+const icon = document.getElementById('hamburger-icon');
 const nav = document.querySelector('.desktop-nav');
-// Add the mobile-nav-item class to nav items
 const navItems = document.querySelectorAll('.nav-item');
+const logo = document.getElementById('logo');
 
-let theme = window.localStorage.getItem("prefered-theme");
-      if (theme) {
-        setTheme(theme);
-      }
-      function setTheme(theme) {
-        document.body.className = theme;
-        window.localStorage.setItem("prefered-theme", theme);
-      }
+let theme = window.localStorage.getItem('prefered-theme');
+if (theme) {
+  setTheme(theme);
+}
+function setTheme(theme) {
+  document.body.className = theme;
+  window.localStorage.setItem('prefered-theme', theme);
+}
 
 function showMobileNav() {
   header.classList.add('mobile-header');
   hamburger.classList.add('mobile-button');
-  if (icon.classList.contains('fa-xmark')) {
-    icon.classList.remove('fa-xmark')
-  }
+  logo.classList.add('hidden');
+  icon.className = 'fa-solid fa-xmark';
   nav.classList.add('mobile-nav');
-  navItems.forEach(item => {
+  navItems.forEach((item) => {
     item.classList.add('mobile-nav-item');
-  })
+  });
 }
 
 function hideMobileNav() {
   header.classList.remove('mobile-header');
   hamburger.classList.remove('mobile-button');
-  icon.className = 'fa-solid fa-xmark';
+  icon.className = 'fa-solid fa-bars';
+  logo.classList.remove('hidden');
   nav.classList.remove('mobile-nav');
-  navItems.forEach(item => {
+  navItems.forEach((item) => {
     item.classList.remove('mobile-nav-item');
-  })
+  });
 }
 
 hamburger.onclick = () => {
@@ -46,4 +42,4 @@ hamburger.onclick = () => {
   } else {
     showMobileNav();
   }
-}
+};
